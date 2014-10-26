3841bf744cb31de779eb20a9c8e69ee6881beec6
Oskar Niburski <oskarniburski@gmail.com>
2014-10-26 15:18:35 -0400

    Initial bundle install

---
+++ react-rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReactRails::Application.config.secret_key_base = '88bcf18b1d8985fc1656c92ab8eea3cd1b3254d665e17ce54537395a7f6126080e864f3f507c4cfc3bb2907db3e5833e4384ad1f403101000dc84e3ddb3401f0'
